e3b095fdbd79a96650bfa318d12cb22967ec575a
Leonardo <leoja00@gmail.com>
2024-06-05 19:17:05 -0300

    musica

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.googlecode.soundlibs:jl:1.0.1.4")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
